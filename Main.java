390eae0b94f4c2bb0567f0cc8f47b4715530a535
LeopoldHolgersson <leopold.holgersson@gmail.com>
2022-11-09 14:32:30 +0100

    Added a new .java class.

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String x = "This is a java-class";
+        String y = "added in a fork from GitHub.";
+
+        System.out.println(x + " " + y);
+    }
+}
